d7ebfa426ec03a04abb31e7c29254a01a936dec2
rajmajster <mpprajski@gmail.com>
2020-09-15 23:35:23 +0200

    Adding plugin

--- lua/hello-nvim.lua
+++ lua/hello-nvim.lua
@@ -1,9 +1,40 @@
 -- hello-nvim.lua
+local api = vim.api
+local buf, win
+
 local function printHelloNvim()
 		print(vim.api.nvim_win_get_width(0))
 		print("hello nvim! aaa")
 end
+		
+local function open_window()
+		buf = api.nvim_create_buf(false,true)
+		local width = api.nvim_get_option("columns")
+		local height = api.nvim_get_option("lines")
+
+		local win_height = math.ceil(height * 0.8 - 4)
+		local win_width = math.ceil(width * 0.8)
+		local row = math.ceil((height - win_height) / 2 - 1)
+		local col = math.ceil((width - win_width) / 2)
+
+		local opts = {
+				style = "minimal",
+				relative = "editor",
+				width = win_width,
+				height = win_height,
+				row = row,
+				col = col
+		}
+
+		win = api.nvim_open_win(buf, true, opts)
+		api.nvim_win_set_option(win, 'cursorline', true) -- it highlight line with the cursor on it
+end
+
+local function test()
+		open_window()
+end
 
 return {
-		printHelloNvim = printHelloNvim
+		printHelloNvim = printHelloNvim,
+		test = test
 }
--- plugin/hello-nvim.vim
+++ plugin/hello-nvim.vim
@@ -4,7 +4,7 @@ if exists('g:loaded_hellonvim') | finish | endif
 let s:save_cpo = &cpo
 set cpo&vim
 
-command! Hellonvim lua require("hello-nvim").printHelloNvim()
+command! Hellonvim lua require("hello-nvim").test()
 
 let &cpo = s:save_cpo
 unlet s:save_cpo
